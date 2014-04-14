fe9bd6d9db93bfcedc45cdfcafa3d734556e5863
mmchugh <michael.mchugh@urbanairship.com>
2014-04-14 17:44:14 -0400

    some build path output and robustness

--- howdoipython/build.py
+++ howdoipython/build.py
@@ -1,13 +1,17 @@
-from os import walk, path
+from os import path, makedirs, walk
 
 from howdoipython.builder import renderer
 
 
 def build_topics(output_path, topics_path='topics'):
     output_path_template = path.join(output_path, 'topics', '{0}.html')
-    full_path = path.join('data', topics_path)
 
-    print output_path, full_path
+    output_dir = path.dirname(output_path_template)
+    if not path.exists(output_dir):
+        print "Making topic output dir at {0}".format(output_dir)
+        makedirs(output_dir)
+
+    full_path = path.join('data', topics_path)
 
     if not path.exists(full_path):
         print "Topics path {0} not found.".format(full_path)
@@ -22,8 +26,10 @@ def build_topics(output_path, topics_path='topics'):
     for topic_file in files:
         topic, extension = path.splitext(topic_file)
         if extension == '.yaml':
-            with open(output_path_template.format(topic), "w") as output_file:
+            file_path = output_path_template.format(topic)
+            with open(file_path, "w") as output_file:
                 output_file.write(renderer.build_topic_html(topic))
+            print "Wrote to {0}".format(file_path)
         else:
             print "Unknown type for topic file: {0}".format(topic_file)
 
