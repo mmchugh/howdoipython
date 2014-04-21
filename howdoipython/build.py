00b478e93548e2ce6e4ce20d8269888795500579
mmchugh <michael.mchugh@urbanairship.com>
2014-04-21 18:22:12 -0400

    add index page

--- howdoipython/build.py
+++ howdoipython/build.py
@@ -3,15 +3,8 @@ from os import path, makedirs, walk
 from howdoipython.builder import renderer, parser
 
 
-def build_topics(output_path, topics_path='topics'):
-    output_path_template = path.join(output_path, 'topics', '{0}.html')
-
-    output_dir = path.dirname(output_path_template)
-    if not path.exists(output_dir):
-        print "Making topic output dir at {0}".format(output_dir)
-        makedirs(output_dir)
-
-    full_path = path.join('data', topics_path)
+def parse_topics():
+    full_path = 'data/topics'
 
     if not path.exists(full_path):
         print "Topics path {0} not found.".format(full_path)
@@ -32,6 +25,17 @@ def build_topics(output_path, topics_path='topics'):
         else:
             print "Unknown type for topic file: {0}".format(topic_file)
 
+    return topic_data
+
+
+def build_topics(output_path, topic_data):
+    output_path_template = path.join(output_path, 'topics', '{0}.html')
+
+    output_dir = path.dirname(output_path_template)
+    if not path.exists(output_dir):
+        print "Making topic output dir at {0}".format(output_dir)
+        makedirs(output_dir)
+
     for topic, data in topic_data.items():
         file_path = output_path_template.format(topic)
         with open(file_path, "w") as output_file:
@@ -39,8 +43,17 @@ def build_topics(output_path, topics_path='topics'):
         print "Wrote to {0}".format(file_path)
 
 
+def build_index(output_path, topic_data):
+    file_path = path.join(output_path, 'index.html')
+    with open(file_path, "w") as output_file:
+        output_file.write(renderer.build_index_html(topic_data))
+    print "Wrote to {0}".format(file_path)
+
+
 def build_all(output_path='output'):
-    build_topics(output_path)
+    topics = parse_topics()
+    build_topics(output_path, topics)
+    build_index(output_path, topics)
 
 if __name__ == '__main__':
     build_all()
--- howdoipython/builder/renderer.py
+++ howdoipython/builder/renderer.py
@@ -5,3 +5,12 @@ env = Environment(loader=PackageLoader('howdoipython', 'templates'))
 def build_topic_html(topic, data):
     template = env.get_template('topic.html')
     return template.render(data)
+
+
+def build_index_html(data):
+    topic_path = 'topics/{0}.html'
+    topics = [
+        {'topic': topic, 'link': topic_path.format(topic)} for topic in data
+    ]
+    template = env.get_template('index.html')
+    return template.render({'topics': topics})
